5f3a03ca73726fd45f32d3e1799a6662da60987a
roshantac <muhammedroshan222@gmail.com>
2020-05-18 00:12:16 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -45,35 +45,9 @@ def predict_img(net,
 
         probs = tf(probs.cpu())
         full_mask = probs.squeeze().cpu().numpy()
-
-    return full_mask > out_threshold
-
-
-def get_args():
-    parser = argparse.ArgumentParser(description='Predict masks from input images',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--model', '-m', default='MODEL.pth',
-                        metavar='FILE',
-                        help="Specify the file in which the model is stored")
-    parser.add_argument('--input', '-i', metavar='INPUT', nargs='+',
-                        help='filenames of input images', required=True)
-
-    parser.add_argument('--output', '-o', metavar='INPUT', nargs='+',
-                        help='Filenames of ouput images')
-    parser.add_argument('--viz', '-v', action='store_true',
-                        help="Visualize the images as they are processed",
-                        default=False)
-    parser.add_argument('--no-save', '-n', action='store_true',
-                        help="Do not save the output masks",
-                        default=False)
-    parser.add_argument('--mask-threshold', '-t', type=float,
-                        help="Minimum probability value to consider a mask pixel white",
-                        default=0.5)
-    parser.add_argument('--scale', '-s', type=float,
-                        help="Scale factor for the input images",
-                        default=0.5)
-
-    return parser.parse_args()
+    full_mask = full_mask - np.min(full_mask)
+    full_mask = full_mask / np.max(full_mask)
+    return full_mask #> out_threshold
 
 
 def get_output_filenames(args):
@@ -96,41 +70,3 @@ def get_output_filenames(args):
 def mask_to_image(mask):
     return Image.fromarray((mask * 255).astype(np.uint8))
 
-
-if __name__ == "__main__":
-    args = get_args()
-    in_files = args.input
-    out_files = get_output_filenames(args)
-
-    net = UNet(n_channels=3, n_classes=1)
-
-    logging.info("Loading model {}".format(args.model))
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    logging.info(f'Using device {device}')
-    net.to(device=device)
-    net.load_state_dict(torch.load(args.model, map_location=device))
-
-    logging.info("Model loaded !")
-
-    for i, fn in enumerate(in_files):
-        logging.info("\nPredicting image {} ...".format(fn))
-
-        img = Image.open(fn)
-
-        mask = predict_img(net=net,
-                           full_img=img,
-                           scale_factor=args.scale,
-                           out_threshold=args.mask_threshold,
-                           device=device)
-
-        if not args.no_save:
-            out_fn = out_files[i]
-            result = mask_to_image(mask)
-            result.save(out_files[i])
-
-            logging.info("Mask saved to {}".format(out_files[i]))
-
-        if args.viz:
-            logging.info("Visualizing results for image {}, close to continue ...".format(fn))
-            plot_img_and_mask(img, mask)
