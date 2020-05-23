02d9003930a8d11f25c381ac65ab7c6c60d114aa
Muhammed Roshan <muhammedroshan222@gmail.com>
2020-05-23 21:43:59 +0530

    Update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -9,6 +9,15 @@ import logging
 from PIL import Image
 
 
+def getData(root,batch_size ,val_percent):
+    dataset = BasicDataset(root)
+    n_val = int(len(dataset) * val_percent)
+    n_train = len(dataset) - n_val
+    train, val = random_split(dataset, [n_train, n_val])
+    train_loader = DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
+    val_loader = DataLoader(val, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True, drop_last=True)
+    return train_loader,val_loader
+
 class BasicDataset(Dataset):
     def __init__(self, root):
         self.root = root
