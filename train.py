07cc62cd339c6864651a84176f1953e7c9bde4b0
Muhammed Roshan <muhammedroshan222@gmail.com>
2020-05-23 23:43:49 +0530

    Update train.py

--- train.py
+++ train.py
@@ -11,50 +11,68 @@ from tqdm import tqdm
 from kornia.losses import SSIM
 from eval import eval_net
 from unet import UNet
-
+import datetime
 from torch.utils.tensorboard import SummaryWriter
 from utils.dataset import BasicDataset
 from torch.utils.data import DataLoader, random_split
 
-def train_net(net, device,train_loader, epochs , lr ):
-    import datetime
-    optimizer = optim.RMSprop(net.parameters(), lr=lr, weight_decay=1e-8, momentum=0.9)
-    #scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=2)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.01)
-    #criterion = nn.BCEWithLogitsLoss()
-    criterion = SSIM(3, reduction = 'mean')
-    n_train =len(train_loader)
-    init = datetime.datetime.now()
-    for epoch in range(epochs):
-        start =  datetime.datetime.now()
-        net.train()
-        epoch_loss = 0  
-        pbar = tqdm(train_loader)
-        #with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{epochs}', unit='img') as pbar:
 
-        for i,batch in enumerate(pbar):
+def test_net(net, device, test_loader):
+    net.eval()
+    test_loss = 0
+    correct = 0
+    start = datetime.datetime.now()
+    with torch.no_grad():
+        for data in test_loader:
             batch['bg'] = batch['bg'].to(device, dtype = torch.float32)
             batch['fgbg'] = batch['fgbg'].to(device, dtype = torch.float32)
             batch['mask'] = batch['mask'].to(device, dtype = torch.float32)
             batch['depth'] = batch['depth'].to(device, dtype = torch.float32) 
-            #depth_pred, masks_pred
             output = net(batch)
             loss1 = criterion(output[1], batch['mask'])
             loss2 = criterion(output[0], batch['depth']) 
-            loss = 2*loss1 + loss2 
-            epoch_loss += loss.item()
-            pbar.set_postfix(desc  = f'Epoch : {epoch+1}  Loss : {loss.item()}  l1: {loss1.item()} l2 = {loss2.item()}')
-            optimizer.zero_grad()
-            loss.backward()
-            nn.utils.clip_grad_value_(net.parameters(), 0.1)
-            optimizer.step()
-        scheduler.step() #loss
-        end=  datetime.datetime.now()
-        print("Time taken for epoch is: ", end-start)
-        print(" Total time taken is : ", end -init)
+            loss = loss1 + loss2 
+            test_loss += loss
+        print("Test loss :",test_loss/len(test_loader.dataset))
+        print(" Time taken for test ", datetime.datetime.now()- start)
         print("ground truth")
         show(batch['depth'].detach().cpu(),nrow=8)
         print("Depth")
         show(output[0].detach().cpu(),nrow=8) # depth
         print("mask")
         show(output[1].detach().cpu(),nrow=8) #mask
+        
+            
+def train_net(net, device,train_loader, epochs , lr, optimizer,scheduler, criterion ):
+    init = datetime.datetime.now()
+    net.train()    
+    pbar = tqdm(train_loader)
+    epoch_loss = 0  
+    start =  datetime.datetime.now()
+    for i,batch in enumerate(pbar):
+        batch['bg'] = batch['bg'].to(device, dtype = torch.float32)
+        batch['fgbg'] = batch['fgbg'].to(device, dtype = torch.float32)
+        batch['mask'] = batch['mask'].to(device, dtype = torch.float32)
+        batch['depth'] = batch['depth'].to(device, dtype = torch.float32) 
+        #depth_pred, masks_pred
+        output = net(batch)
+        loss1 = criterion(output[1], batch['mask'])
+        loss2 = criterion(output[0], batch['depth']) 
+        loss = 2*loss1 + loss2 
+        epoch_loss += loss.item()
+        pbar.set_postfix(desc  = f'Epoch : {epoch+1}  Loss : {loss.item()}  l1: {loss1.item()} l2 = {loss2.item()}')
+        optimizer.zero_grad()
+        loss.backward()
+        nn.utils.clip_grad_value_(net.parameters(), 0.1)
+        optimizer.step()
+        
+    
+    end=  datetime.datetime.now()
+    print("Time taken for Training 1 epoch is: ", end-start)
+
+#         print("ground truth")
+#         show(batch['depth'].detach().cpu(),nrow=8)
+#         print("Depth")
+#         show(output[0].detach().cpu(),nrow=8) # depth
+#         print("mask")
+#         show(output[1].detach().cpu(),nrow=8) #mask
