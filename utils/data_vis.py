fcf04775a8603ac3ab99781642f4ce96eecd56ee
Muhammed Roshan <muhammedroshan222@gmail.com>
2020-05-23 21:50:38 +0530

    Update data_vis.py

--- utils/data_vis.py
+++ utils/data_vis.py
@@ -1,5 +1,37 @@
+
+import torchvision
 import matplotlib.pyplot as plt
 
+def show(tensors, figsize= (10,10), *args, **kwargs):
+  try:
+    tensors = tensors.detach().cpu()
+  except:
+    pass
+  grid_tensor = torchvision.utils.make_grid(tensors, *args, **kwargs)  
+  grid_image  = grid_tensor.permute(1, 2, 0)
+  plt.figure(figsize = figsize)
+  plt.imshow(grid_image)
+  plt.xticks([])  
+  plt.yticks([])
+  plt.show()
+
+def show_pred(tensors, *args, **kwargs):
+  tensors = (tensors * std[None, : , None, None]) + mean[None, :, None, None]
+  show(tensors, *args, **kwargs)
+ 
+def draw_and_save(tensors, name, figsize=(20,20),*args,**kwargs):
+  try:
+    tensors = tensors.detach().cpu()
+  except:
+    pass
+  grid_tensor = torchvision.utils.make_grid(tensors, *args, **kwargs)  
+  grid_image  = grid_tensor.permute(1, 2, 0)
+  plt.figure(figsize = figsize)
+  plt.imshow(grid_image)
+  plt.xticks([])  
+  plt.yticks([])
+  plt.savefig(name,bbox_inches='tight')
+  plt.close()
 
 def plot_img_and_mask(img, mask):
     classes = mask.shape[2] if len(mask.shape) > 2 else 1
